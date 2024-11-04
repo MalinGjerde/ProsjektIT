f1569da8b66d4d455517f2533a2498edca5da408
Stianb94 <Stianbj94@gmail.com>
2024-11-04 13:09:34 +0100

    Laget program
    Lagt til program

---
+++ Sentral_Program/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sentral_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
